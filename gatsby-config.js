464f23faefa6aa59fbb87615483b270bbc330505
Yomi Eluwande <iamlese@yahoo.com>
2020-07-13 15:23:17 +0200

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,13 +60,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: ` Yomi Eluwande`,
+        short_name: `ye`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `static/favicon.jpg`,
       },
     },
     `gatsby-plugin-react-helmet`,
