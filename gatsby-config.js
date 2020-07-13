279b77cfc29a703fe674cfeeb358ecdab273656d
Yomi Eluwande <iamlese@yahoo.com>
2020-07-13 10:16:45 +0200

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Yomi Eluwande`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Yomi Eluwande`,
+      summary: `Frontend Engineer living in Berlin`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `My personal website.`,
+    siteUrl: `http://yomieluwande.me`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `yomieluwande`,
     },
   },
   plugins: [
